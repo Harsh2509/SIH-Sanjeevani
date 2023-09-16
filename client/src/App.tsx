e20483d1121e43e0f616853904cc228f815168fe
Harsh <harshchauhan6176@gmail.com>
2023-09-16 11:16:10 +0530

    updating code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,20 @@ import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Signin from './components/Signin'
+
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/"  element={<Home/>} />
+          <Route path="/signin" element={<Signin/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
