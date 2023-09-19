3df3b039b3a5c57c8b0ce98de45b34f639dc0c00
adarsh8469 <123470349+adarsh8469@users.noreply.github.com>
2023-09-19 20:28:43 +0530

    Fixed the bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
-import IQ_Page from "./components/IQ_Page";
 import Details from "./components/Personal";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -19,7 +18,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/iqpage" element={<IQ_Page />} />
           <Route path="/details" element={<Details />} />
           <Route path="/games" element={<GameHome />} />
           <Route path="/games/*" element={<Games />} />
