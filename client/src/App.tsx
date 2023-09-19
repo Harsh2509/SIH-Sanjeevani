a9baa553c1ae4f7b441707c66d9f0acdfdb920b8
Harsh <harshchauhan6176@gmail.com>
2023-09-19 10:26:38 +0530

    Bubble_Game added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,39 @@
-import './App.css'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Login from './components/Login'
-import IQ_Page from './components/IQ_Page'
-import Details from './components/Personal'
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import IQ_Page from "./components/IQ_Page";
+import Details from "./components/Personal";
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Signin from './components/Signin'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./components/Signin";
+import BubbleGame from "./games/Bubble_Game/game"; // Corrected the import name
 
 function App() {
-
   return (
-    <>
-      <Router>
+    <Router>
+      <>
         <Navbar />
         <Routes>
-          <Route path="/"  element={<Home/>} />
-          <Route path="/signin" element={<Signin/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/iqpage" element={<IQ_Page/>} />
-          <Route path="/details" element={<Details/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/iqpage" element={<IQ_Page />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/games/*" element={<Games />} />
         </Routes>
-      </Router>
-    </>
-  )
+      </>
+    </Router>
+  );
 }
 
-export default App
+function Games() {
+  return (
+    <Routes>
+      <Route path="/bubble-game" element={<BubbleGame />} />{" "}
+      {/* Corrected the component name */}
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/games/Bubble_Game/game.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect } from "react";
+import "./game.css";
+export default function Game1() {
+  useEffect(() => {
+    let score = 0;
+    let time = 60;
+    let hitrn = 0;
+
+    function increaseScore() {
+      score += 10;
+      const scoreElement = document.querySelector("#scoreval");
+      if (scoreElement !== null) scoreElement.textContent = String(score);
+    }
+    function MakeBubble() {
+      let clutter = "";
+
+      for (let i = 1; i < 75; i++) {
+        const rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
+      }
+      const pbtmElement = document.querySelector("#pbtm");
+      if (pbtmElement !== null) pbtmElement.innerHTML = clutter;
+    }
+
+    function MakeHit() {
+      hitrn = Math.floor(Math.random() * 10);
+      const hitbtnElement = document.querySelector("#hitbtn");
+      if (hitbtnElement !== null) hitbtnElement.textContent = String(hitrn);
+    }
+
+    function runTimer() {
+      const timeEnd = setInterval(function () {
+        if (time > 0) {
+          time--;
+          const timeIntervalElement = document.querySelector("#timeInterval");
+          if (timeIntervalElement !== null)
+            timeIntervalElement.innerHTML = String(time);
+        } else {
+          clearInterval(timeEnd);
+          const pbtmElement = document.querySelector("#pbtm");
+          if (pbtmElement !== null)
+            pbtmElement.innerHTML = `<h1>Your Score is ${score}</h1>`;
+        }
+      }, 1000);
+    }
+
+    const pbtmElement = document.querySelector("#pbtm");
+    if (pbtmElement !== null)
+      pbtmElement.addEventListener("click", function (dets) {
+        const clickedElement = dets.target as HTMLElement;
+        const clickedTextContent = clickedElement.textContent;
+        const clickednum = Number(clickedTextContent);
+        if (hitrn == clickednum) {
+          increaseScore();
+          MakeHit();
+          MakeBubble();
+        }
+      });
+
+    MakeHit();
+    runTimer();
+    MakeBubble();
+  }, []);
+
+  return (
+    <>
+      <div id="main">
+        <div id="panel">
+          <div id="ptop">
+            <div className="elem">
+              <h2>Hit</h2>
+              <div id="hitbtn" className="box">
+                0
+              </div>
+            </div>
+            <div className="elem">
+              <h2>Timer</h2>
+              <div id="timeInterval" className="box">
+                60
+              </div>
+            </div>
+            <div className="elem">
+              <h2>Score</h2>
+              <div id="scoreval" className="box">
+                0
+              </div>
+            </div>
+          </div>
+          <div id="pbtm"></div>
+        </div>
+      </div>
+    </>
+  );
+}
