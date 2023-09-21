b1300bb5c0dc69dcfb700483e59ea2c340be64cc
Harsh <harshchauhan6176@gmail.com>
2023-09-21 11:45:33 +0530

    some shemas added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,33 @@
 //import React from 'react'
-import "./Navbar.css"
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <nav>
-          <ul className="nav-links">
-              <img src="/public/logo.png" alt="logo" height={50}/>
-              <a href="#"><li>Services</li></a>
-              <a href="#"><li>Pricing</li></a>
-              <a href="#"><li>About Us</li></a>
-              <a href="#"><li>Contact Us</li></a>
-              <a href="#"><li>Sign in / Sign up</li></a>
-              <a href="#"><li>English 🌐</li></a>
-              <i className="fa-solid fa-magnifying-glass"></i>
-          </ul>
+        <ul className="nav-links">
+          <img src="/public/logo.png" alt="logo" height={50} />
+          <a href="#">
+            <li>Services</li>
+          </a>
+          <a href="#">
+            <li>Pricing</li>
+          </a>
+          <a href="#">
+            <li>About Us</li>
+          </a>
+          <a href="#">
+            <li>Contact Us</li>
+          </a>
+          <a href="#">
+            <li>Sign in / Sign up</li>
+          </a>
+          <a href="#">
+            <li>English 🌐</li>
+          </a>
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </ul>
       </nav>
     </div>
-  )
+  );
 }
--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,1 +1,111 @@
 import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+function emailValidator(email: string): boolean {
+  return /^[a-zA-Z0-9_\-]+@[a-zA-Z0-9_\-]+\.[a-zA-Z]{2,4}$/.test(email);
+}
+
+enum Gender {
+  male = "male",
+  female = "female",
+  other = "other",
+}
+
+enum Level {
+  one = 1,
+  two,
+  three,
+  four,
+  five,
+}
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      required: true,
+      trim: true,
+      validate: {
+        validator: emailValidator,
+        message: "Invalid Email passed to mongoose. (coming from UserSchema)",
+      },
+    },
+    password: {
+      type: String,
+      minLength: 8,
+      required: true,
+    },
+    gameScore: {
+      type: Number,
+      default: 0,
+    },
+    stread: {
+      type: Number,
+      default: 0,
+    },
+    dob: {
+      type: Date,
+    },
+    guardian: [
+      {
+        relation: String,
+        name: String,
+        location: String,
+        phoneNo: String,
+        email: {
+          type: String,
+          validate: {
+            validator: emailValidator,
+            message:
+              "Invalid Email passed to mongoose. (coming from UserSchema)",
+          },
+        },
+      },
+    ],
+    gender: {
+      type: String,
+      enum: [Gender.male, Gender.female, Gender.other],
+    },
+    level: {
+      type: Number,
+      enum: [Level.one, Level.two, Level.three, Level.four, Level.five],
+      default: Level.one,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/** -------------------------------Game Schema---------------------------------------- */
+
+const gameSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  description: {
+    type: String,
+    maxLength: 1000,
+  },
+  route: {
+    type: String,
+    required: true,
+    maxLength: 200,
+  },
+  src: {
+    type: String,
+    required: true,
+  },
+  targets: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
